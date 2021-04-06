3e68c2a455a84b01ab3be368ef490c3b13551d76
Alain <alain@kinomap.net>
2021-04-06 18:23:52 +0200

    Correction tp4

--- src/question2/Cooking.java
+++ src/question2/Cooking.java
@@ -1,5 +1,13 @@
 package question2;
 
 public class Cooking {
+    private RiceType riceType;
 
+    Cooking(RiceType riceType) {
+        this.riceType = riceType;
+    }
+
+    public String prepareDinner() {
+        return "I am done cooking " + this.riceType;
+    }
 }
